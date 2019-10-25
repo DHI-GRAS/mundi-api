6694520f314549a7e3868616b87aebda4bc9075b
anbr <aborgstad@gmail.com>
2019-10-25 13:02:16 +0200

    Update docstring to Numpy style

--- mundi_api/download.py
+++ mundi_api/download.py
@@ -2,27 +2,32 @@ import os
 import shutil
 import tempfile
 from contextlib import closing
-from os.path import getsize
-import threading
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from urllib.parse import urlparse
 import requests
 from tqdm import tqdm
 
-
-pbar = tqdm()
+PBAR = tqdm()
 
 
 def download(url, outfile=None, workdir=None):
     """Downloads a file to the given location.
     This function stores unfinished downloads in the given working directory.
-    Parameters:
-        :param url: Mundi DIAS URL to download.
-        :param workdir: Path where incomplete downloads are stored.
-        :param outfile: Output filename or absolute path output file.
-    """
 
+    Parameters
+    ----------
+    url:
+        Mundi DIAS URL to download.
+    outfile:
+        Output filename or absolute path output file
+    workdir:
+        Path where incomplete downloads are stored.
+
+    Returns
+    -------
+    None
+    """
     filename = Path(urlparse(url).path).name
     outfile = _format_path(outfile)
     if os.path.isdir(outfile):
@@ -49,11 +54,20 @@ def _format_path(path):
 
 def download_list(urls, outdir=None, workdir=None, threads=3):
     """Downloads a list of URLs
-    File names are [UID].zip
-    :param urls: A list of URLs.
-    :param outdir: Output direcotry.
-    :param workdir: Storage of temporary files
-    :param threads: Number of simultaneous downloads.
+
+    Parameters
+    ----------
+    urls:
+        Downloads a list of URLs.
+    outdir:
+        Output direcotry.
+    workdir:
+        Storage of temporary files.
+    threads:
+        Number of simultaneous downloads.
+    Returns
+    -------
+    None
     """
     pool = ThreadPool(threads)
     download_lambda = lambda x: download(x, outfile=outdir, workdir=workdir)
@@ -61,21 +75,19 @@ def download_list(urls, outdir=None, workdir=None, threads=3):
 
 
 def _download_raw_data(url, path):
-    already_downloaded_bytes = getsize(path)
     downloaded_bytes = 0
-    headers = {"Range": "bytes={}-".format(already_downloaded_bytes)}
     with closing(
             requests.get(url, stream=True, timeout=10)
-    ) as r, tqdm(
+    ) as req, tqdm(
         unit='B',
         unit_scale=True
     ) as progress:
         chunk_size = 2 ** 20  # download in 1 MB chunks
         i = 0
-        with open(path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=chunk_size):
+        with open(path, 'wb') as openfile:
+            for chunk in req.iter_content(chunk_size=chunk_size):
                 if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
+                    openfile.write(chunk)
                     progress.update(len(chunk))
                     downloaded_bytes += len(chunk)
                     i += 1
