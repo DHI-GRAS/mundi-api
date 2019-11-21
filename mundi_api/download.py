6234c65282f7527146a70c2b3e74bd06d66b7d01
Pantelis Kouris <ppkouris@gmail.com>
2019-11-21 16:32:57 +0100

    Save files in a folder

--- mundi_api/download.py
+++ mundi_api/download.py
@@ -65,10 +65,9 @@ def download_from_s3(url, access_key, secret_key, target_path):
     path_split = urlparse(url).path.split('/')
     bucket = path_split[1]
     prefix = '/'.join(path_split[2:])
-    print(bucket)
-    print(prefix)
+    product_folder = path_split[:-1]
     storage_client = S3Storage(s3_client)
-    storage_client.download_product(bucket, prefix, target_path)
+    storage_client.download_product(bucket, prefix, os.path.join(target_path, product_folder))
 
 
 def _format_path(path):
